ca5146c5a3606b182acff8130de6e48fe87aa1ad
Tyler <tsm33@hw.ac.uk>
2016-11-05 21:34:27 +0000

    Update sphere.py

--- sphere.py
+++ sphere.py
@@ -1,39 +1,16 @@
-
-import numpy as np
-import scipy
-import matcompat
+#Simple implementation of a 2 dimensional sphere function
+#Used to generate data in a format ready to be input into the neural network
+import numpy
 import random
-anslist = []
-# if available import pylab (from matlibplot)
-try:
-    import matplotlib.pylab as plt
-except ImportError:
-    pass
+def sphere(x,y):
+	print"X and Y and answer \n", [[x, y], [x**2 + y**2]]
 
-def sphere(x):
-    global anslist
-    # Local Variables: y, x, s, j, n
-    # Function calls: sphere
-    #% 
-    #% Sphere function 
-    #% Matlab Code by A. Hedar (Nov. 23, 2005).
-    #% The number of variables n should be adjusted below.
-    #% The default value of n = 30.
-    #% 
-    n = 2.
-    s = 0.
-    for j in np.arange(1., (n)+1):
-        s = s+x[int(j)-1]**2.
-        print "X",x, "\n"   
-    y = s
-    #print random.randint(1,30)
-    
-    print "Ans", y
-
-#Generate some dummy datacd Doc*/
-dumblist = []
-for _ in range(0, 30):
-    dumblist.append([random.uniform(0.1, 0.5),random.uniform(0.1, 0.5)])
-print dumblist
-for i in range(0, len(dumblist)):
-    sphere(dumblist[i])
+bestlist = []
+sphere(0.1,0.2)
+sphere(0.2,0.3)
+sphere(0.3,0.4)
+sphere(0.4,0.5)
+sphere(0.5,0.6)
+sphere(0.1,0.5)
+sphere(0.2,0.4)
+sphere(0.3, 0.5)

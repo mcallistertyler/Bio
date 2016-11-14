da700bca2a99dc10aab4a4f2d81a98c966cdc130
Tyler <tsm33@hw.ac.uk>
2016-11-14 02:26:04 +0000

    Updated genetic.py
    Finalised version of the neural network and genetic algorithm. Not connected to COCO

--- genetic.py
+++ genetic.py
@@ -3,6 +3,12 @@ import random
 import string
 import numpy as np
 
+#Global variables used to hold various information
+#taken from the population of neural networks created
+#Such as the best MLP that has run so far
+#The difference in input answers from expected
+#The current best cost
+#The current best weights
 listofbests = []
 totaldifference = []
 dic = {}
@@ -11,9 +17,8 @@ bestWO = []
 betterdifference = 0
 bettercost = 1000
 savedNN = {}
-bestNN = {}
 
-
+#Dummy data used for raw testing
 sets = [ [[0,0], [0.0]],
          [[0.1, 0.1], [0.02]],
          [[0.1,0.2], [0.05]],
@@ -21,11 +26,16 @@ sets = [ [[0,0], [0.0]],
          [[0.1,0.3], [0.10]],
          [[0.2,0.3], [0.13]]
     ]
-#sets2 = [(1,2), (2,3)]
-sets2 = [(0 , 0), (0.1 , 0.1), (0.1 , 0.2), (0.2 , 0.2), (0.1 , 0.3), (0.2 , 0.3)]
-#Sphere function
-#Take some arbitary input
-#Return it as an input for our Neural Network
+#Example data to be input into the functions to produce sample answers
+#Used to test large quantities of differing inputs
+#sets2 = [(0, 0), (0.01397, 0.01489151), (0.0148941, 0.0221964), (0.0201549, 0.021564), (0.015555,0.0212), (0.0201560001, 0.03029741), (0.0251560, 0.0235450), (0.03161321, 0.0296594), (0.02058564,0.0156445), (0.012454, 0.0153489), (0.013446545, 0.0248915)]
+sets2 = [(0, 0),(0.1, 0.15),(0.2, 0.2), (0.25, 0.15), (0.3, 0.3), (0.35, 0.25)]
+
+#Used for testing completely random inputs in large quantities
+#sets2 = [(random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5)), (random.uniform(0, 0.5), random.uniform(0, 0.5))]
+
+#All continuous optimizer functions
+#Used to test the constraints on the neural network
 def sphere(sets):
     ans = 0
     newlist = []
@@ -44,10 +54,10 @@ def rastrigin(sets):
         ans = 20
         newlist.append([list(sets[x])])
         for y in range(0,len(sets[x])):
-            ans = ans + (sets[x][y]**2 - 10*math.cos(2 * math.pi * sets[y][x]))
-            if y == 1:
-                newlist[x].append([ans])
-            ans = 0
+        	ans = ans + (sets[x][y]**2 - 10*math.cos(2 * math.pi * sets[x][y]))
+        	if y == 1:
+        		newlist[x].append([ans])
+        		ans = 0
     return newlist
 
 def rosenbrock(sets):
@@ -56,7 +66,6 @@ def rosenbrock(sets):
     for x in range(0,len(sets)):
         newlist.append([list(sets[x])])
         for y in range(0,len(sets[x])):
-            print sets[x][y]
             if y == 0:
                 ans = 100*(sets[x][y+1] - sets[x][y]**2)**2 + ((sets[x][y] - 1)**2)
             if y == 1:
@@ -64,38 +73,70 @@ def rosenbrock(sets):
                 
     return newlist
 
+def schnefel(sets):
+	ans = 0
+	newlist = []
+	for x in range(0,len(sets)):
+		newlist.append([list(sets[x])])
+		for y in range(0, len(sets[x])):
+			if y == 0:
+				ans = 418.9829 * 2 - sets[x][0] * math.sin(math.sqrt(abs(sets[x][0]))) + sets[x][1] * math.sin(math.sqrt(abs(sets[x][1])))
+				newlist[x].append([ans])
+			ans = 0
+	return newlist
+
+def diffpowers(sets):
+	print 
+	ans = 0
+	newlist = []
+	for x in range(0, len(sets)):
+		newlist.append([list(sets[x])])
+		for y in range(0, len(sets[x])):
+			if y == 0:
+				ans = math.sqrt(abs(sets[x][0])**(2+4*(0/1)) + abs(sets[x][1])**(2+4*(1/1)))
+				newlist[x].append([ans])
+			ans = 0
+	return newlist
 
 
-
-def makeMatrix(I, J, fill=0.0):
+def makeMatrix(I, J):
+	#Make a 2D list from two inputs
     m = []
     for i in range(I):
-        m.append([fill]*J)
+        m.append([0.0]*J)
     return m
 
+#Various activation functions
 def ReLU(x):
         return x * (x > 0)
 
 def tanh(x):
     return np.tanh(x)
 
+#Derivative sigmoid function
+#Unused in GA but can be used in backpropagation
 def dsigmoid(y):
     return 1.0 - y**2
 
 class NN:
     def __init__(self, inputlayer, hiddenlayer, outputlayer):
+    	#Initialize class variables
+    	#Number of neurons
         self.inputlayer = inputlayer
         self.hiddenlayer = hiddenlayer
         self.outputlayer = outputlayer
-
+        #Activation functions
         self.activateInput = [1.0]*self.inputlayer
         self.activateHidden = [1.0]*self.hiddenlayer
         self.activateOutput = [1.0]*self.outputlayer
-        
+        #Weights which will be set during the start of the Genetic algorithm
         self.wi = []
         self.wo = []
 
-    def update(self, inputs):
+    def feedForward(self, inputs):
+    	#Forward pass through the input layer to the hidden layer
+    	#Pass from the hidden layer to the output layer using a chosen
+    	#activation function. Then return our answers
         for i in range(self.inputlayer):
             self.activateInput[i] = inputs[i]
 
@@ -103,7 +144,7 @@ class NN:
             sum = 0.0
             for i in range(self.inputlayer):
                 sum = sum + self.activateInput[i] * self.wi[i][j]
-            self.activateHidden[j] = tanh(sum)
+            self.activateHidden[j] = ReLU(sum)
 
         for k in range(self.outputlayer):
             sum = 0.0
@@ -114,34 +155,38 @@ class NN:
         return self.activateOutput
 
     def test(self, patterns):
+    	#Print out the training inputs, the values
+    	#given by the neural network and the expected values to reach
         global allTests
         x = 0
         for p in patterns:
-            x = self.update(p[0])
+            x = self.feedForward(p[0])
             print p[0], "->", x[0], ", Expected Result : ", p[1][0]
 
     def finaltest(self, patterns):
+    	#Run final test to return relevant outputs to COCO
         global allTests
         x = 0
         finalAnswers = []
         for p in patterns:
-            x = self.update(p[0])
+            x = self.feedForward(p[0])
             print p[0],  x[0], "Expected : ", p[1][0]
             finalAnswers.append(x[0])
         return finalAnswers
 
     def costfunction(self, actual, expected):
+    	#Calculate the cost of our neural network
+    	#The smaller the cost the closer we are to our desired answers
         global totaldifference
         totaldifference = []
         difference = actual[0] - expected[0]
-        print "DIFFERENCE", difference
         totaldifference.append(difference)
         averagediff = sum(totaldifference) / len(totaldifference)
-        # Made a change here to stop taking average difference
         dic['AvgDiff'] = averagediff
         return (difference)**2 * 0.5
-
-    def train(self, patterns, iterations=1, N=0.5, M=0.1):
+    #Dictionary created allows me to easily store important information
+    #About each Neural Network
+    def train(self, patterns, iterations=1):
         dic['InputWeights'] = self.wi
         dic['OutputWeights'] = self.wo
         findCost = []
@@ -149,20 +194,15 @@ class NN:
             for p in patterns:
                 inputs = p[0]
                 targets = p[1]
-                self.update(inputs)
-                dic['Cost'] = self.costfunction(self.update(inputs),targets)
+                self.feedForward(inputs)
+                dic['Cost'] = self.costfunction(self.feedForward(inputs),targets)
                 findCost.append(dic['Cost'])
         dic['AvgCost'] = sum(findCost) / len(findCost)
 
 
-def training():
-    global sets
-    n = NN(2, 4, 1)
-    n.train(sphere(sets2))
-    n.test(sphere(sets2))
-
-
 def mutateI(inputlayer, hiddenlayer, generation, difference, cost):
+	#Set the weights to random values if the current best value has not been initialized
+	#Otherwise mutate the best weights we have
     global bettercost
     learningRate = 0.1
     if generation == 0:
@@ -185,7 +225,7 @@ def mutateI(inputlayer, hiddenlayer, generation, difference, cost):
 
 def mutateO(hiddenlayer, outputlayer, generation, difference, cost):
     #Set weights to random if the bestweight value has not been initialized
-    #Otherwise mutate weights
+    #Otherwise mutate the weights
     global bettercost
     learningRate = 0.1
     if generation < 1:
@@ -209,11 +249,10 @@ def mutateO(hiddenlayer, outputlayer, generation, difference, cost):
 
 def Genetically(generations, population, sets2):
     global sets, bestWI, bestWO, bettercost, betterdifference
-
+    #Set the number of nodes at each layer here
     inputlayer = 2
-    hiddenlayer = 4
+    hiddenlayer = 3
     outputlayer = 1
-    bestNN = 0
     bestCost = 0
     smallestCost = []
     for t in range(0, generations):
@@ -221,16 +260,16 @@ def Genetically(generations, population, sets2):
         nn2 = NN(inputlayer,hiddenlayer,outputlayer)
         #Clear saved NNs for new generation
         savedNN['Saved'] = []
-        #Population is 500 Neural Betworks
+        #Population is 500 Neural Networks
         for x in range(0, population):
             print "Generation: ", t
             nn2.wi = mutateI(inputlayer, hiddenlayer, t, betterdifference, bettercost)
             nn2.wo = mutateO(hiddenlayer, outputlayer, t, betterdifference,bettercost)
             nn2.train(sphere(sets2))
             nn2.test(sphere(sets2))
+            #Save relevant information into a dictionary
             savedNN['Saved'].append([dic['AvgCost'],dic['InputWeights'],dic['OutputWeights'],dic['AvgDiff']])
-        #Find NN with the smallest cost
-        #Save it's weights and rerun
+        	#Find NN with the smallest cost and save its weights
             smallestCost = min(savedNN['Saved'])
             bettercost = smallestCost[0]
             bestWI = smallestCost[1]
@@ -238,6 +277,8 @@ def Genetically(generations, population, sets2):
             betterdifference = smallestCost[3]
         listofbests.append((t, smallestCost))
     if t == generations - 1:
-        print"Finished! Returning answers to examplexperiment.py..."
+        print"Finished! Final generations best member..."
         return nn2.finaltest(sphere(sets2))
+
+#Generations and population and the training input set can be changed here
 Genetically(200, 500, sets2)
